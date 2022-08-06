c66914e39006f699f224e141f3569c5d09c15aa4
shakil <shakilnirob111@gmail.com>
2022-08-07 01:56:41 +0600

    first commit

---
+++ Constant.java
@@ -0,0 +1,11 @@
+public abstract class Constant {
+    public static String textFile = "students.txt";
+    public static String validText = "Please Enter a valid Code to Execute!";
+    public static  String validText2 = "Plz Enter a valid Code to Execute." +
+            "\nThe given Code should Contain only one word." +
+            "\nExample: java NameOfTheFile (a/r/c)";
+    public static String loadingText = "Loading data ...";
+    public static String dataLoaded = "Data Loaded.";
+    public static String dataFound = "We found it!";
+    public static String dataNotFound = "Data Can't be load now!";
+}
